384f4515b1f246449917ce4c48c1604dd850d55c
ryan11js <104442987+ryan11js@users.noreply.github.com>
2024-09-29 12:51:17 -0400

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return "<a href='/about'>Click here for the about page</a>"
+
+
+@app.route('/about')
+def about():
+    return """
+    <p1>about this project</p>
+    <a href='/'>back to home</a>
+    """
 
 if __name__ == '__main__':
+
     app.run()
