78c91d08fda7e937edba765fd75e2e39f3c54b9f
ryan11js <104442987+ryan11js@users.noreply.github.com>
2024-10-09 18:30:15 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function swipeLeft() {
   console.log("Swiped Left");
-  // Add logic here for going to the previous food item
+  // to be continued
 }
 
 function swipeRight() {
   console.log("Swiped Right");
-  // Add logic here for going to the next food item
+  // also to be continued
 }
 
